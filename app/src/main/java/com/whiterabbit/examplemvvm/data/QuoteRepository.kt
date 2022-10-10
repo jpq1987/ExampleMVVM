d4225c33a34a84e0d4979a177e69c917064d1d12
Juan Pablo Quiñones <juanpq@MBP-de-Juan.fibertel.com.ar>
2022-10-10 11:50:19 -0300

    Add Dependency Injection

--- app/src/main/java/com/whiterabbit/examplemvvm/data/QuoteRepository.kt
+++ app/src/main/java/com/whiterabbit/examplemvvm/data/QuoteRepository.kt
@@ -3,14 +3,16 @@ package com.whiterabbit.examplemvvm.data
 import com.whiterabbit.examplemvvm.data.model.QuoteModel
 import com.whiterabbit.examplemvvm.data.model.QuoteProvider
 import com.whiterabbit.examplemvvm.data.network.QuoteService
+import javax.inject.Inject
 
-class QuoteRepository {
-
-    private val api = QuoteService()
+class QuoteRepository @Inject constructor(
+    private val api: QuoteService,
+    private val quoteProvider: QuoteProvider
+) {
 
     suspend fun getAllQuotes(): List<QuoteModel> {
         val response = api.getQuotes()
-        QuoteProvider.quotes = response
+        quoteProvider.quotes = response
         return response
     }
 }
--- app/src/main/java/com/whiterabbit/examplemvvm/domain/GetRandomQuoteUseCase.kt
+++ app/src/main/java/com/whiterabbit/examplemvvm/domain/GetRandomQuoteUseCase.kt
@@ -2,11 +2,12 @@ package com.whiterabbit.examplemvvm.domain
 
 import com.whiterabbit.examplemvvm.data.model.QuoteModel
 import com.whiterabbit.examplemvvm.data.model.QuoteProvider
+import javax.inject.Inject
 
-class GetRandomQuoteUseCase {
+class GetRandomQuoteUseCase @Inject constructor(private val quoteProvider: QuoteProvider) {
 
     operator fun invoke(): QuoteModel? {
-        val quotes = QuoteProvider.quotes
+        val quotes = quoteProvider.quotes
         if (!quotes.isNullOrEmpty()) {
             val randomNumber = (quotes.indices).random()
             return quotes[randomNumber]
